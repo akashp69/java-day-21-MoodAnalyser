6222093047d78e0836f4139a7b744dc15b18e186
akashp69 <akashpakhare499@gmail.com>
2022-09-04 23:20:35 +0530

    Added: Program for check return HAPPY

--- src/test/java/com/bridgelabz/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -1,14 +1,22 @@
 package com.bridgelabz;
 
-import org.junit.Test;
+import com.bridgelabz.MoodAnalyser;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertSame;
 
 public class MoodAnalyserTest {
     @Test
-    public void givenSadMoodShouldReturnSad(){
-        MoodAnalyser analyse=new MoodAnalyser();
-        String mood=analyse.analyseMood("I am in sad mood");
+    public void givenSadMoodShouldReturnSad() {
+        MoodAnalyser analyse = new MoodAnalyser();
+        String mood = analyse.analyseMood("I am in sad mood");
         assertSame("SAD", mood);
+    }
+
+    @Test
+    public void givenAnyMoodShouldReturnHappy() {
+        MoodAnalyser analyse = new MoodAnalyser();
+        String mood = analyse.analyseMood("I am in any mood");
+        assertSame("HAPPY", mood);
     }
 }
