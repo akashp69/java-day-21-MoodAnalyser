bbb02154bfdbff54626575115506f48b5c2396f9
akashp69 <akashpakhare499@gmail.com>
2022-09-04 22:56:16 +0530

    Added: Program for analyse Happy or Sad Mood

--- src/main/java/com/bridgelabz/MoodAnalyser.java
+++ src/main/java/com/bridgelabz/MoodAnalyser.java
@@ -1,4 +1,18 @@
 package com.bridgelabz;
 
 public class MoodAnalyser {
+
+    public MoodAnalyser() {}
+    public String analyseMood(String message) {
+        if (message.contains("sad")) {
+            return "SAD";
+        }else
+            return "HAPPY";
+    }
+
+    public static void main(String[] args) {
+        MoodAnalyser mood = new MoodAnalyser();
+        System.out.println(mood.analyseMood("i am sad"));
+        System.out.println(mood.analyseMood("i am happy"));
+    }
 }
